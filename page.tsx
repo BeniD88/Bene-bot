11220410c0ed7c34e493b66b399b1e86e094db21
BeniD88 <turibenedikt007@gmail.com>
2025-05-18 14:01:39 +0200

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Chatbot</h1>
+      <p>This is the mobile-friendly chatbot prototype.</p>
+    </main>
+  );
+}
